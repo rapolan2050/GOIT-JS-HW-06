1cfba093c4010765b28fba13654df8d114f4ad2a
Raul Camacho <elaragat2050@gmail.com>
2023-10-03 14:22:35 -0500

    sincro prueba

---
+++ java.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-constant-condition */
+
+/* for (let monigote = 0; monigote >= 12; monigote = monigote + 2); {
+    console.log(monigote);
+} */
+
+if (false != true) {
+    console.log('That makes sense.');
+    if (1 < 2) {
+        console.log('No surprise there.');
+    }
+}
